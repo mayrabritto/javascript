4e1a32e4c25bd914a66c090518145f7d33a729ee
mayrabritto <71741193+mayrabritto@users.noreply.github.com>
2020-11-27 21:52:57 +0000

    criando-novos-exercicios
    exercicios 16 e 17

---
+++ curso javascript/aula 015/ex016/script.js
@@ -0,0 +1,12 @@
+function contar() {
+    var ini = document.getElementById('txti')
+    var fim = document.getElementById('txtf')
+    var passo = document.getElementById('txtp')
+    var res = document.getElementById('res')
+
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        window.alert('[ERRO!] Verifique os valores digitados')
+    } else {
+        res.innerHTML = ('Contando...')
+    }
+}
