0d863edb5c1664f41175b1035081d82905289a2a
mayrabritto <71741193+mayrabritto@users.noreply.github.com>
2020-12-06 17:58:22 +0000

    exercicio16
    realizando exercicio de contagem js

--- curso javascript/aula 015/ex016/script.js
+++ curso javascript/aula 015/ex016/script.js
@@ -6,7 +6,27 @@ function contar() {
 
     if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
         window.alert('[ERRO!] Verifique os valores digitados')
+        res.innerHTML = `Impossível contar!!`
     } else {
-        res.innerHTML = ('Contando...')
+        res.innerHTML = ('Contando... <br>')
+        var i = Number(ini.value)
+        var f = Number(fim.value) 
+        var p = Number(passo.value)
+        if (p <= 0) {
+            alert('Passo inválido! Considerando passo igual a 1!!')
+            p = 1
+        }
+
+        if (i < f) {
+            for(var c = i; c <= f; c += p) {
+                res.innerHTML += `${c} \u{1F449}`
+            }
+            
+        } else {
+            for(var c = i; c >= f; c -= p) {
+                res.innerHTML += `${c} \u{1F449}`
+            }
+        }
+        res.innerHTML += `\u{1F3C1}`
     }
 }
